583f26cf502490b4ab2359fda6585fa9ed64d4f7
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-08-01 14:25:32 +0530

    Created graphics.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from graphics import Window, Line, Point
+        
+
+def main():
+    win = Window(800,600)
+    line = Line(Point(50,50), Point(400,400))
+    win.draw_line(line)
+    win.wait_for_close()
+    
+main()
