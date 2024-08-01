5b32f1ffbfb5722806aafb11955953ced357515f
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-08-01 16:06:04 +0530

    added maze

---
+++ maze.py
@@ -0,0 +1,55 @@
+
+from cell import Cell
+import time,random
+
+class Maze:
+    def __init__(self,
+                x1,
+                y1,
+                num_rows,
+                num_cols,
+                cell_size_x,
+                cell_size_y,
+                win,
+                ) -> None:
+        
+        self._x1 = x1
+        self._y1 = y1
+        self._num_row = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+        
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
+        
+        
+    
+    def _draw_cell(self,i,j):
+        if self._win is None:
+            return
+        x1 = self._x1 + (i*self._cell_size_x)
+        y1 = self._y1 + (j*self._cell_size_y)
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1,y1,x2,y2)
+        self._animate()
+    
+    def _create_cells(self):
+        for col in range(0,self._num_cols):
+            column = []
+            for row in range(0,self._num_row):
+                column.append(Cell(self._win))
+            self._cells.append(column)
+            
+        for x in range(0,self._num_cols):
+            for y in range(0,self._num_row):
+                self._draw_cell(x,y)
+                
+    
