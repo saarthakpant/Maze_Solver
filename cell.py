d39862f5d1567d87e9e6178ad3c9581bfc7b1027
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-08-01 16:11:49 +0530

    added Tests

--- cell.py
+++ cell.py
@@ -1,6 +1,6 @@
 from graphics import Window, Line, Point
 class Cell:
-    def __init__(self,win: Window) -> None:
+    def __init__(self,win: Window = None) -> None:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
